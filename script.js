63d68d8ba21a8bb037c4c8717a132c21f827993d
Tyler <tknotts@gmail.com>
2017-01-28 15:08:38 -0800

    Finished

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
 $(document).ready(function() {
-
-  // insert everything into an array
+  listSpots();
   function listSpots() {
-
     $.getJSON("topspots.json", function(data) { // pulling in the data
-      console.log(data);
-
-      // loop through the data, row by row
-      $.each(data, function(spotName, spotDescription, spotLink) {
-        console.log(data);
-
-        // throw the html concatenated with the variables into a variable (currentRow)
-        var currentRow = "<tr><td>" + spotName + "</td><td>" + spotDescription + "</td><td><a href='https://www.google.com/maps?q=" + spotLink + "'><button id='submitButton' class='btn btn - primary'></a></td></tr>";
-
-        console.log(currentRow);
-
-        //display the current info in a row
-        $(currentRow).AppendTo('#goHere');
+      $.each(data, function(index, item) {
+        var currentRow = "<tr><td>" + item.name + "</td><td>" + item.description + "</td><td><a href='https://www.google.com/maps?q=" + item.location + "'><button id='submitButton' class='btn btn - primary'>Open in Maps</button></a></td></tr>";
+        $(currentRow).appendTo('#goHere');
       });
     });
   };
-});
-
-
-
-
-
-/**
- * my api key
- * AIzaSyAFeBwJ_f8CPObDU2ZeQDcVb2SEcJqdrc4
-**/
+})
