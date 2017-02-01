984b15771a07f059ed037efa821495584a1695c1
Tyler <tknotts@gmail.com>
2017-01-31 21:02:57 -0800

    finished extra credit

--- script.js
+++ script.js
@@ -1,50 +1,37 @@
 $(document).ready(function() {
+  var waypointsArray = [];
+
   function listSpots() {
-    var waypointsArray = [];
-    $.getJSON("topspots.json", function(data) { // pulling in the data
+    $.getJSON("topspots.json", function(data) {
       $.each(data, function(index, item) {
 
         // concat the table cell HTML and variables
         var currentRow = "<tr><td>" + item.name + "</td><td>" + item.description + "</td><td><a href='https://www.google.com/maps?q=" + item.location + "'><button id='submitButton' class='btn btn - primary'>Open in Maps</button></a></td></tr>";
         $(currentRow).appendTo('#goHere');
 
-        // store coordinates in an array
-        var i = i + 1;
-        waypointsArray[i] = item.location;
-
+        // store coordinates in an array for later
+        waypointsArray[index] = item.location;
+        if (data.length == index + 1) {
+          makeMap(waypointsArray);
+        };
       });
     });
-    showMap(waypointsArray);
   };
   listSpots();
 });
 
 
+function makeMap(waypointsArray) {
+  console.log("right inside makemap" + waypointsArray)
+  var linkFront = "<img width='600' src='https://maps.googleapis.com/maps/api/staticmap?center=San+Diego,CA&zoom=13&size=600x300&maptype=roadmap&markers=color:blue";
+  var linkCoords = waypointsArray.join('%7Clabel:C%7C');
+  var linkApiKey = "AIzaSyB5sDZfu9lScUfVkb1TkdWge18P_YrWCL8'>";
+  var linkComplete = linkFront + linkCoords + linkApiKey;
+  showMap(linkComplete);
+}
 
-function showMap(waypointsArray) {
 
-  console.log(waypointsArray[4]);
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: new google.maps.LatLng(32.71, -117.16),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-
-  var infowindow = new google.maps.InfoWindow();
-
-  var marker, i;
-
-  for (i = 0; i < waypointsArray.length; i++) {
-    marker = new google.maps.Marker({
-      position: new google.maps.LatLng(waypointsArray[i]),
-      map: map
-    });
-
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-      return function() {
-        infowindow.setContent(waypointsArray[i]);
-        infowindow.open(map, marker);
-      }
-    })(marker, i));
-  }
+function showMap(linkComplete) {
+  console.log("The completed link: " + linkComplete);
+  $('#myDiv').html(linkComplete);
 }
